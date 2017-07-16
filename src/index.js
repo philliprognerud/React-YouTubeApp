b672a7bb7495d200ab197223282c43625c9f7c5d
Phillip Rognerud <philliprognerud2@yahoo.com>
2017-07-16 09:29:49 +0000

    added youtube api data

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from "youtube-api-search";
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyCX-J5kMpP3I4NcvKU63S7bsyRkvRCqprE';
+
+YTSearch({key: API_KEY, term: 'pewdiepie'}, function(data){
+    console.log(data);
+});
 
 const App = () => {
     return (
