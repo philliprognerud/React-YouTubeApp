8dacb7959adb9b246106d39e7e2bd86ca35963cd
Phillip Rognerud <philliprognerud2@yahoo.com>
2017-07-18 02:13:00 +0000

    added video list items

--- src/index.js
+++ src/index.js
@@ -1,21 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from "youtube-api-search";
-
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list'
 const API_KEY = 'AIzaSyCX-J5kMpP3I4NcvKU63S7bsyRkvRCqprE';
 
-YTSearch({key: API_KEY, term: 'pewdiepie'}, function(data){
-    console.log(data);
-});
 
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+class App extends Component {
+    constructor(props){
+        super(props);
+        
+        this.state = { videos: [] };
+        
+        YTSearch({key: API_KEY, term: 'pewdiepie'}, (videos) => {
+            this.setState({ videos });
+        });
+    }
+    
+    render(){
+        return (
+            <div>
+                <SearchBar />
+                <VideoList videos={this.state.videos} />
+            </div>
+        );
+    }
 }
 
 ReactDOM.render(<App />, document.querySelector(".container"));
